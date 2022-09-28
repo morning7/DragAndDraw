1f4427d55a11d4c6a9fb80bfec03a637344f2703
huangjiachen@phoenixtv.com <hjc123456>
2022-09-28 11:39:12 +0800

    GestureDetector用法

--- app/src/main/java/com/karson/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/karson/draganddraw/BoxDrawingView.kt
@@ -7,8 +7,11 @@ import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
+import kotlin.math.abs
 
 private const val TAG = "BoxDrawingView"
 
@@ -25,6 +28,88 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
     private val backgroundPaint = Paint().apply {
         color = 0xfff8efe0.toInt()
     }
+
+//    private lateinit var currentPoint: PointF
+//
+//    private val gestureListener = object : GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+//        override fun onDown(event: MotionEvent): Boolean {
+//            currentBox = Box(currentPoint)
+//            boxen.add(currentBox!!)
+//            return true
+//        }
+//
+//        override fun onShowPress(e: MotionEvent?) {
+//            Log.i(TAG, "onShowPress")
+//        }
+//
+//        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+//            Log.i(TAG, "onSingleTapUp")
+//            return false
+//        }
+//
+//        override fun onScroll(
+//            e1: MotionEvent?,
+//            e2: MotionEvent?,
+//            distanceX: Float,
+//            distanceY: Float
+//        ): Boolean {
+//            Log.i(TAG, "onScroll")
+//            updateCurrentBox(currentPoint)
+//            return true
+//        }
+//
+//        override fun onLongPress(e: MotionEvent) {
+//            Log.i(TAG, "onLongPress" + e.action)
+//        }
+//
+//        val FLING_MIN_DISTANCE = 100
+//        val FLING_MIN_VELOCITY = 200
+//
+//        override fun onFling(
+//            e1: MotionEvent,
+//            e2: MotionEvent,
+//            velocityX: Float,
+//            velocityY: Float
+//        ): Boolean {
+//            Log.i(TAG, "onFling")
+//            if (e1.x - e2.x > FLING_MIN_DISTANCE && abs(velocityX) > FLING_MIN_VELOCITY) {
+//                Log.i(TAG, "onFling left")
+//            } else if (e2.x - e1.x > FLING_MIN_DISTANCE && abs(velocityX) > FLING_MIN_VELOCITY) {
+//                Log.i(TAG, "onFling right")
+//            }
+//            updateCurrentBox(currentPoint)
+//            currentBox = null
+//            return true
+//        }
+//
+//        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+//            Log.i(TAG, "onSingleTapConfirmed")
+//            return true
+//        }
+//
+//        override fun onDoubleTap(e: MotionEvent): Boolean {
+//            Log.i(TAG, "onDoubleTap")
+//            return true
+//        }
+//
+//        override fun onDoubleTapEvent(e: MotionEvent): Boolean {
+//            Log.i(TAG, "onDoubleTap")
+//            return true
+//        }
+//
+//    }
+//
+//    private val gestureDetector = GestureDetectorCompat(context, gestureListener)
+//
+//    init {
+//        gestureDetector.setIsLongpressEnabled(false)
+//    }
+//
+//    @SuppressLint("ClickableViewAccessibility")
+//    override fun onTouchEvent(event: MotionEvent): Boolean {
+//        currentPoint = PointF(event.x, event.y)
+//        return gestureDetector.onTouchEvent(event)
+//    }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -67,8 +152,10 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
         super.onDraw(canvas)
         canvas.drawPaint(backgroundPaint)
         boxen.forEach { box ->
-            Log.i(TAG, "boxen ${box.top} ${box.bottom} ${boxen.size}")
+//            Log.i(TAG, "boxen ${box.top} ${box.bottom} ${boxen.size}")
             canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
         }
     }
+
+
 }
